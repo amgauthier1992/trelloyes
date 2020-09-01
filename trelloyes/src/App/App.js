4d1ae63ff8792a9634e37cecca0265e0806e4228
Andy Gauthier <amgauthier1992@gmail.com>
2020-09-01 16:27:54 -0400

    almost there

--- trelloyes/src/App/App.js
+++ trelloyes/src/App/App.js
@@ -2,4 +2,32 @@ import React from "react";
 import "./App.css";
 import List from "./../List/List";
 
-//iterate through lists rendering a list component for each list
+class App extends React.Component {
+  render() {
+    return (
+      <main className="App">
+        <header className="App-header">
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className="App-list">
+          {this.props.store.lists.map((list, index) => {
+            return (
+              <section key={index}>
+                <List header={list.header} cards={list.cardIds} />
+              </section>
+            );
+          })}
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
+
+//Line 15- iterate through store.lists array rendering a list component for each list(total of 4 in the array).
+//Passing in the prop values of list.header and list.cardIds to the list component.
+
+//also, you're going to want to pass more props to List, if you want access to the cards
+
+//so you'll need to find a way to use the cardIds to get the full list of cards
--- trelloyes/src/List/List.js
+++ trelloyes/src/List/List.js
@@ -1,16 +1,36 @@
 import React from "react";
 import "./list.css";
+
 import Card from "./../Card/Card";
 
-function List(props) {
-  return (
-    <section className="List">
-      <header className="List-header">
-        <h2>{props.header}</h2>
-      </header>
-      <div className="List-cards"></div>
-    </section>
-  );
+class List extends React.Component {
+  render() {
+    console.log(this.props.cards);
+    return (
+      <section className="List">
+        <header className="List-header">
+          <h2>{this.props.header}</h2>
+        </header>
+        <div className="List-cards">
+          {this.props.cards.map((card, index) => {
+            return (
+              <div key={index}>
+                <Card />
+              </div>
+            );
+          })}
+          <button
+            type="button"
+            className="List-add-button"
+            // onClick={this.props.addCardClicked}
+          >
+            + Add Random Card
+          </button>
+        </div>
+      </section>
+    );
+  }
 }
 
+export default List;
 //iterate through cards rendering a card component

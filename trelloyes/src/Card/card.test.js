2ffa4fdc109fc0d88efbac2c2f564954fb072259
Andy Gauthier <amgauthier1992@gmail.com>
2020-09-03 15:50:23 -0400

    finished tests

--- trelloyes/src/Card/card.test.js
+++ trelloyes/src/Card/card.test.js
@@ -6,12 +6,14 @@ import Card from "./Card";
 describe("Card component", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<Card />, div);
+    ReactDOM.render(<Card title={"First Card"} content={"lorem ipsum"} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it("renders the UI as expected", () => {
-    const tree = renderer.create(<Card />).toJSON();
+    const tree = renderer
+      .create(<Card title={"First Card"} content={"lorem ipsum"} />)
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
--- trelloyes/src/List/list.test.js
+++ trelloyes/src/List/list.test.js
@@ -2,20 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
 import List from "./List";
-import App from "./App";
 
-describe("List component", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("section");
-    ReactDOM.render(<List header={list.header}
-        cards={list.cardIds.map(
-          (id) => this.props.store.allCards[id]
-        )/>, section);
-    ReactDOM.unmountComponentAtNode(section);
-  });
+// Smoke test
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <List
+      header={"Header"}
+      cards={[
+        { id: "a", title: "First card", content: "lorem ipsum" },
+        { id: "b", title: "Second card", content: "lorem ipsum" },
+        { id: "c", title: "Third card", content: "lorem ipsum" },
+      ]}
+    />,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
 
-  it("renders the UI as expected", () => {
-    const tree = renderer.create(<List />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+// Snapshot
+it("renders a dummy list of cards as expected", () => {
+  const tree = renderer
+    .create(
+      <List
+        header={"Header"}
+        cards={[
+          { id: "a", title: "First card", content: "lorem ipsum" },
+          { id: "b", title: "Second card", content: "lorem ipsum" },
+          { id: "c", title: "Third card", content: "lorem ipsum" },
+        ]}
+      />
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
